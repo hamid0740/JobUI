b2b494729f70c5b8b8b47bc0c5d0efe694e5cfd8
hamid0740 <hamidd0740@gmail.com>
2020-08-12 14:52:15 +0430

    Added 2 new MissionTypes

--- src/hamid0740/JobUI/Main.php
+++ src/hamid0740/JobUI/Main.php
@@ -1,13 +1,14 @@
 <?php
 
- /*
- * The original EconomyJob is written by OneBone
- * Used to FormAPI Library virion by jojoe77777
- * added the UI and make it Advanced by hamid0740
- * Discord: hamid0740#3725
- * Github: https://github.com/hamid0740/
- * Telegram & Instagram: @hamid0740
- */
+	/*
+	* The original EconomyJob is written by OneBone
+	* Used the FormAPI Library virion by jojoe77777
+	* Added the UI and make it Advanced by hamid0740
+	* Discord: hamid0740#3725
+	* Github: https://github.com/hamid0740/
+	* Poggit: https://poggit.pmmp.io/p/JobUI
+	* Telegram & Instagram: @hamid0740
+	*/
 
 namespace hamid0740\JobUI;
 
@@ -15,9 +16,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\entity\Animal;
+use pocketmine\entity\Monster;
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\permission\Permission;
@@ -50,10 +56,8 @@ class Main extends PluginBase implements Listener{
 		$this->saveDefaultConfig();
 		$this->saveResource("jobs.yml");
 		$this->saveResource("messages.yml");
-		$this->saveResource("buttons.yml");
 		$this->jobs = new Config($this->getDataFolder() . "jobs.yml", Config::YAML);
 		$this->messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
-		$this->buttons = new Config($this->getDataFolder() . "buttons.yml", Config::YAML);
 		$this->player = new Config($this->getDataFolder() . "players.yml", Config::YAML);
 
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -91,20 +95,6 @@ class Main extends PluginBase implements Listener{
 		}
 		return false;
 	}
-	
-	/**
-	* Get JobUI buttons
-	*
-	* @param string $id
-	*
-	* @return string | bool
-	*/
-	public function getButton($id){
-		if($this->buttons->exists($id)){
-			return $this->buttons->get($id);
-		}
-		return false;
-	}
 
 	/**
 	 * @priority LOWEST
@@ -117,9 +107,9 @@ class Main extends PluginBase implements Listener{
 
 		$job = $this->jobs->get($this->player->get($player->getName()));
 		if($job !== false){
-			if(isset($job[$block->getID().":".$block->getDamage().":break"])){
-				$money = $job[$block->getID().":".$block->getDamage().":break"];
-				if ($player->hasPermission("jobui.earn.break")) {
+			if(isset($job["Mission"][$block->getID().":".$block->getDamage().":Break"])){
+				$money = $job["Mission"][$block->getID().":".$block->getDamage().":Break"];
+				if ($player->hasPermission("jobui.earn.break") or $player->hasPermission("jobui.*")) {
 					if($money > 0){
 						$this->api->addMoney($player, $money);
 						$player->sendPopup($this->getMessage("earn-popup-1") . $money . EconomyAPI::getInstance()->getMonetaryUnit() . $this->getMessage("earn-popup-2"));
@@ -144,9 +134,9 @@ class Main extends PluginBase implements Listener{
 
 		$job = $this->jobs->get($this->player->get($player->getName()));
 		if($job !== false){
-			if(isset($job[$block->getID().":".$block->getDamage().":place"])){
-				$money = $job[$block->getID().":".$block->getDamage().":place"];
-				if ($player->hasPermission("jobui.earn.place")) {
+			if(isset($job["Mission"][$block->getID().":".$block->getDamage().":Place"])){
+				$money = $job["Mission"][$block->getID().":".$block->getDamage().":Place"];
+				if ($player->hasPermission("jobui.earn.place") or $player->hasPermission("jobui.*")) {
 					if($money > 0){
 						$this->api->addMoney($player, $money);
 						$player->sendPopup($this->getMessage("earn-popup-1") . $money . EconomyAPI::getInstance()->getMonetaryUnit() . $this->getMessage("earn-popup-2"));
@@ -159,7 +149,73 @@ class Main extends PluginBase implements Listener{
 			}
 		}
 	}
-
+	
+	/**
+	 * @priority LOWEST
+	 * @ignoreCancelled true
+	 * @param EntityDeathEvent $event
+	 */
+	public function onMobDeath(EntityDeathEvent $event){
+		$entity = $event->getEntity();
+		$cause = $entity->getLastDamageCause();
+		if($cause instanceof EntityDamageByEntityEvent){
+			$player = $cause->getDamager();
+			if($player instanceof Player){
+				$job = $this->jobs->get($this->player->get($player->getName()));
+				if($job !== false){
+					if(!$entity instanceof Player){
+						if(isset($job["Mission"]["Hunter"])){
+							$money = $job["Mission"]["Hunter"];
+							if ($player->hasPermission("jobui.earn.hunter") or $player->hasPermission("jobui.*")) {
+								if($money > 0){
+									$this->api->addMoney($player, $money);
+									$player->sendPopup($this->getMessage("earn-popup-1") . $money . EconomyAPI::getInstance()->getMonetaryUnit() . $this->getMessage("earn-popup-2"));
+								}else{
+									$this->api->reduceMoney($player, $money);
+								}
+							}else{
+								$player->sendPopup($this->getMessage("hunter-noperm-popup"));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @priority LOWEST
+	 * @ignoreCancelled true
+	 * @param PlayerDeathEvent $event
+	 */
+	public function onPlayerDeath(PlayerDeathEvent $event){
+		$entity = $event->getEntity();
+		$cause = $entity->getLastDamageCause();
+		if($cause instanceof EntityDamageByEntityEvent){
+			$player = $cause->getDamager();
+			if($player instanceof Player){
+				$job = $this->jobs->get($this->player->get($player->getName()));
+				if($job !== false){
+					if($entity instanceof Player){
+						if(isset($job["Mission"]["Murderer"])){
+							$money = $job["Mission"]["Murderer"];
+							if ($player->hasPermission("jobui.earn.murderer") or $player->hasPermission("jobui.*")) {
+								if($money > 0){
+									$this->api->addMoney($player, $money);
+									$player->sendPopup($this->getMessage("earn-popup-1") . $money . EconomyAPI::getInstance()->getMonetaryUnit() . $this->getMessage("earn-popup-2"));
+								}else{
+									$this->api->reduceMoney($player, $money);
+								}
+							}else{
+								$player->sendPopup($this->getMessage("murderer-noperm-popup"));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	/**
 	 * @return Main
 	*/
@@ -187,7 +243,7 @@ class Main extends PluginBase implements Listener{
 			if(!$sender instanceof Player){
 				$sender->sendMessage("Please run this command in-game.");
 			}else{
-				if ($sender->hasPermission("jobui.command.job")) {
+				if ($sender->hasPermission("jobui.command.job") or $player->hasPermission("jobui.*")) {
 					$this->FormJob($sender);
 				}else{
 					$sender->sendMessage("§7[§6JobUI§7] " . "§cYou can't join a job in this world");
@@ -198,7 +254,7 @@ class Main extends PluginBase implements Listener{
 			if(!$sender instanceof Player){
 				$sender->sendMessage("Please run this command in-game.");
 			}else{
-				if ($sender->hasPermission("jobui.command.retire")){
+				if ($sender->hasPermission("jobui.command.retire") or $player->hasPermission("jobui.*")){
 					if($this->player->exists($sender->getName())){
 						$job = $this->player->get($sender->getName());
 						$this->player->remove($sender->getName());
@@ -229,7 +285,12 @@ class Main extends PluginBase implements Listener{
 					break;
 					
 					case "2";
-					$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("myjob-message") . $this->player->get($player->getName()));
+					if($this->player->exists($player->getName())){
+						$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("myjob-message") . $this->player->get($player->getName()));
+					}else{
+						$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("nojob-message"));
+					}
+					
 					break;
 					
 					case "3";
@@ -244,7 +305,11 @@ class Main extends PluginBase implements Listener{
 				}
 			});
 			$form->setTitle($this->getMessage("title-mainui"));
-			$form->setContent($this->getMessage("myjob-text-mainui")  . $this->player->get($player->getName()));
+			if($this->player->exists($player->getName())){
+				$form->setContent($this->getMessage("myjob-text-mainui") . $this->player->get($player->getName()));
+			}else{
+				$form->setContent($this->getMessage("nojob-text-mainui"));
+			}			
 			$form->addButton($this->getMessage("jobjoin-button-mainui"));
 			$form->addButton($this->getMessage("jobsinfo-button-mainui"));
 			$form->addButton($this->getMessage("myjob-button-mainui"));
@@ -262,10 +327,14 @@ class Main extends PluginBase implements Listener{
 			foreach($this->jobs->getAll() as $name => $job){
 				switch($data){
 					case "$i";
-						$this->player->set($player->getName(), "$name");
-						$job = $this->player->get($player->getName());
-						$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("jobjoin-message") . $job);
-						break;
+						if ($player->hasPermission($job["Permission"]) or $player->hasPermission("jobui.job.*") or $player->hasPermission("jobui.*")){
+							$this->player->set($player->getName(), "$name");
+							$job = $this->player->get($player->getName());
+							$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("jobjoin-message") . $job);
+							break;
+						}else{
+							$player->sendMessage("§7[§6JobUI§7] " . $this->getMessage("jobjoin-noperm-message") . $name);
+						}
 					}
 				$i++;
 				}
@@ -275,11 +344,7 @@ class Main extends PluginBase implements Listener{
 			$form->setTitle($this->getMessage("title-jobjoinui"));
 			
 		foreach($this->jobs->getAll() as $name => $job){
-			if($this->buttons->exists("name-" . $name) and $this->buttons->exists("image-type-" . $name) and $this->buttons->exists("image-" . $name)){
-				$form->addButton($this->getButton("name-" . $name), $this->getButton("image-type-" . $name), $this->getButton("image-" . $name));
-			}else{
-				$form->addButton($this->getMessage("color-jobsname-jobjoinui") . $name);
-			}
+			$form->addButton($job["Button-Name"], $job["Image-Type"], $job["Image"]);
 		}
 			
 			$player->sendForm($form);
@@ -296,9 +361,15 @@ class Main extends PluginBase implements Listener{
 				break;
 			}
 		});
-		$form->setTitle($this->getMessage("title-jobsinfoui"));
-		$form->setContent($this->getMessage("text-jobsinfoui"));
-		$form->addButton($this->getMessage("exit-button-jobsinfoui"));
+		$form->setTitle($this->getMessage("title-myjobinfoui"));
+		
+		if($this->player->exists($player->getName())){
+			$job = $this->jobs->get($this->player->get($player->getName()));
+			$form->setContent($job["Info"]);
+		}else{
+			$form->setContent($this->getMessage("nojob-text-myjobinfoui"));
+		}
+		$form->addButton($this->getMessage("exit-button-myjobinfoui"));
 		$player->sendForm($form);
 	}
 }
